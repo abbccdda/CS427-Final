6dc5a0bdd82d7a7234bd6cdea20481b3c3de39e3
Boyang Chen <bchen11@illinois.edu>
2014-11-13 13:54:40 -0600

    add new UC39 testcases

--- unittests/edu/ncsu/csc/itrust/dao/transaction/TransactionDAOTest.java
+++ unittests/edu/ncsu/csc/itrust/dao/transaction/TransactionDAOTest.java
@@ -1,13 +1,17 @@
 package edu.ncsu.csc.itrust.dao.transaction;
 
 import java.util.Date;
+import java.util.List;
+
 import junit.framework.TestCase;
+import edu.ncsu.csc.itrust.beans.TransactionBean;
+import edu.ncsu.csc.itrust.dao.DAOFactory;
 import edu.ncsu.csc.itrust.dao.mysql.TransactionDAO;
 import edu.ncsu.csc.itrust.exception.DBException;
 import edu.ncsu.csc.itrust.testutils.EvilDAOFactory;
-
-public class TransactionDAOExceptionTest extends TestCase {
+public class TransactionDAOTest extends TestCase {
 	private TransactionDAO evilDAO = EvilDAOFactory.getEvilInstance().getTransactionDAO();
+	private TransactionDAO normalDAO = DAOFactory.getProductionInstance().getTransactionDAO();
 
 	@Override
 	protected void setUp() throws Exception {
@@ -30,15 +34,17 @@ public class TransactionDAOExceptionTest extends TestCase {
 			assertEquals(EvilDAOFactory.MESSAGE, e.getSQLException().getMessage());
 		}
 	}
-
-	public void testRecordAccessesException() throws Exception {
-		try {
-			evilDAO.getRecordAccesses(0L, -1, new Date(), new Date(), false);
-			fail("DBException should have been thrown");
-		} catch (DBException e) {
-			assertEquals(EvilDAOFactory.MESSAGE, e.getSQLException().getMessage());
+	
+	public void testInValidTransaction() throws Exception{
+		List<TransactionBean> result = null;
+		try{
+			normalDAO.getLog("tester", "tester", "LOGIN_SUCCESS", new Date(),new Date());	
+		}	
+		catch(DBException e){
+			String err = "Context Lookup Naming Exception: Need to specify class name in environment or system property,"
+					+ " or as an applet parameter,"
+					+ " or in an application resource file:  java.naming.factory.initial";
+			assertEquals(err, e.getSQLException().getMessage());
 		}
 	}
-	
-	
-}
+}	
